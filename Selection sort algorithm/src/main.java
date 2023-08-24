fed46f5e0107f36069402974eaa4334d16f6cce8
Truong Manh Thang <126144891+cccccc9712@users.noreply.github.com>
2023-08-24 12:25:03 +0700

    J1.S.P0002

---
+++ Selection sort algorithm/src/main.java
@@ -0,0 +1,71 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ *
+ * @author tmtmt
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Unsorted Array: [");
+        for (int i = 0; i < n; i++) {
+            arr[i] = createRandomNumber();
+            if (i == n - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.print("]\n");
+        selectionSort(arr);
+        System.out.print("Sorted Array: [");
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    public static int createRandomNumber() {
+        Random generator = new Random();
+        int value = generator.nextInt(8) + 1;
+        return value;
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
